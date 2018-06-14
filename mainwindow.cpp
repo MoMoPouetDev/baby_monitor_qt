ed717248774f58ed01302f1c66c13477925bcdc1
morgan <morgan.venandy@gmail.com>
2018-06-14 17:34:34 +0200

    add signal and slot on video and icone for button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,17 @@
-#include <QString>
 #include <stdio.h>
-#include <QDebug>
-
+#include <QIcon>
+#include <QSize>
+#include <QString>
 #include "mainwindow.h"
 
 MainWindow::MainWindow() : QWidget()
 {
     setFixedSize(480, 320);
 
-    m_buttonPower = new QPushButton("OFF", this);
-    m_buttonPower -> move(380,10);
-    m_buttonPower -> setFixedWidth(80);
+    m_buttonPower = new QPushButton(this);
+    m_iconPower.addFile(m_pathIconPower);
+    m_buttonPower->setIcon(m_iconPower);
+    m_buttonPower->setGeometry(420, 10, 50, 30);
 
     m_buttonUp = new QPushButton("+", this);
     m_buttonUp -> move(380, 230);
@@ -27,9 +28,9 @@ MainWindow::MainWindow() : QWidget()
     m_progressSound = new QProgressBar(this);
     m_progressSound -> setGeometry(405, 50, 30, 160);
     m_progressSound -> setOrientation(Qt::Vertical);
-    m_progressSound -> setValue(m_soundValue);
-    m_progressSound -> setStyleSheet(m_style);
-    m_progressSound -> setTextVisible(false);
+    m_progressSound -> setValue(0);
+    m_progressSound -> setStyleSheet(getStyle());
+    m_progressSound -> setTextVisible(true);
 
     QObject::connect(m_buttonUp, SIGNAL(clicked()), this, SLOT(buttonPlus()));
     QObject::connect(m_buttonDown, SIGNAL(clicked()), this, SLOT(buttonMinus()));
@@ -37,38 +38,81 @@ MainWindow::MainWindow() : QWidget()
     QObject::connect(m_buttonPower, SIGNAL(clicked()), this, SLOT(buttonPower()));
 
     m_player = new QMediaPlayer(this, QMediaPlayer::VideoSurface);
+    QObject::connect(m_player, SIGNAL(volumeChanged(int)), this, SLOT(changeVolumeBar(int)));
+    QObject::connect(m_player, SIGNAL(mutedChanged(bool)), this, SLOT(muteVolumeBar(bool)));
     m_video = new QVideoWidget(this);
 
-    m_player -> setMedia(QUrl::fromLocalFile("/Users/morganvenandy/test1.mp4"));
+    m_player -> setMedia(QUrl("rtsp://root:bni@192.168.0.90/axis-media/media.amp?camera=1"));
     m_player -> setVideoOutput(m_video);
     m_video -> setGeometry(10,10, 360, 280);
     m_player -> play();
+
+    m_player->setVolume(50);
 }
 
 MainWindow::~MainWindow()
 {
 
 }
+
 void MainWindow::buttonPlus(void)
 {
-    if(m_soundValue <= SOUND_MAX)
+    if(m_player->isMuted())
     {
-        m_soundValue += 10;
-        m_progressSound->setValue(m_soundValue);
+        m_player->setMuted(false);
+        m_buttonSound->setText("Mute");
+    }
+    if( m_player->volume() <= SOUND_MAX)
+    {
+        m_player->setVolume((m_player->volume())+SOUND_MARGIN);
     }
 }
 
 void MainWindow::buttonMinus(void)
 {
-
+    if(m_player->isMuted())
+    {
+        m_player->setMuted(false);
+        m_buttonSound->setText("Mute");
+    }
+    if(m_player->volume() >= SOUND_MIN)
+    {
+        m_player->setVolume((m_player->volume())-SOUND_MARGIN);
+    }
 }
 
 void MainWindow::buttonMute(void)
 {
-
+    if(!m_player->isMuted())
+    {
+        m_player->setMuted(true);
+        m_buttonSound->setText("Unmute");
+    }
+    else
+    {
+        m_player->setMuted(false);
+        m_buttonSound->setText("Mute");
+    }
 }
 
 void MainWindow::buttonPower(void)
 {
 
+}
+
+void MainWindow::changeVolumeBar(int volumeValue)
+{
+    m_progressSound->setValue(volumeValue);
+}
+
+void MainWindow::muteVolumeBar(bool muted)
+{
+    if(muted)
+    {
+        m_progressSound->setValue(SOUND_MIN);
+    }
+    else
+    {
+        m_progressSound->setValue(m_player->volume());
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,11 +4,22 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QIcon>
+#include <QSize>
 #include <QProgressBar>
 #include <QMediaPlayer>
 #include <QVideoWidget>
 
-#define SOUND_MAX   100
+#ifdef __APPLE__
+    #define VIDEO_URL   "/Users/morganvenandy/test1.mp4"
+#elif __linux__
+    #define VIDEO_URL   "/home/morgan/test.mp4"
+    #define PATH_ICON_POWER "/home/morgan/git/baby_monitor_qt/images/power.png"
+#endif
+
+#define SOUND_MAX       100
+#define SOUND_MIN       0
+#define SOUND_MARGIN    10
 
 class MainWindow : public QWidget
 {
@@ -23,17 +34,27 @@ class MainWindow : public QWidget
     void buttonMinus(void);
     void buttonMute(void);
     void buttonPower(void);
+    void changeVolumeBar(int);
+    void muteVolumeBar(bool);
 
     private:
-    int m_soundValue = 50;
+    QString m_style = "QProgressBar {border: 2px solid grey; border-radius: 5px;} QProgressBar::chunk {background-color: #4EE34E; height: 10px; margin: 0.5px;}";
+    QString m_pathIconPower = PATH_ICON_POWER;
+    QIcon m_iconPower;
+    QSize m_sizeIcon = QSize(50, 50);
     QPushButton *m_buttonPower,
                 *m_buttonUp,
                 *m_buttonDown,
                 *m_buttonSound;
-    QString m_style = "QProgressBar {border: 2px solid grey; border-radius: 5px;} QProgressBar::chunk {background-color: #CD96CD; height: 10px; margin: 0.5px;}";
     QProgressBar *m_progressSound;
     QMediaPlayer *m_player;
     QVideoWidget *m_video;
+
+    QString getStyle()
+    {
+        return m_style;
+    }
+
 };
 
 #endif // MAINWINDOW_H
